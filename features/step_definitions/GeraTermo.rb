a1098aa5ba5ccd97ff3fba941d87f59edf5a5509
“LucasKoritar23 <lucas.gimenes23@gmail.com>
2018-04-27 11:47:22 -0300

    Troca de Projeto

---
+++ features/step_definitions/GeraTermo.rb
@@ -0,0 +1,24 @@
+require 'faker'
+link = "http://exec-dev01.sma.local/sigam-adequacao-test"
+Dado("que esteja tela de cadastro do Sare") do
+   #link = "http://exec-dev01.sma.local/sma-est-car_test/"
+   visit(link)
+   RealizaLogin.acesso(usuario = "leilacm")
+   find(:id,"ctl00_lnkLogo").click
+   find("[src='imagens/logo/SARE.png']").click
+   find(:id, "ctl00_conteudo_ctl00_rptrMenu_ctl00_imgLogo").click
+  end
+  
+  Quando("preencher todos os campos do projeto") do
+    PreencheSare.CadastroInicial(nomeSare = "Projeto " +  Faker::Name.first_name, link)
+    PreencheSare.Pessoa(nomPessoa = Faker::Name.name , funcaoPessoa = "Compromissário", tipoPessoa = "Jurídica")
+    PreencheSare.Pessoa(nomPessoa = Faker::Name.name , funcaoPessoa = "Compromissário", tipoPessoa = "Física")
+    PreencheSare.Pessoa(nomPessoa = Faker::Name.name , funcaoPessoa = "Representante legal", tipoPessoa = "Física")
+    PreencheSare.DesenhaPropSare
+    PreencheSare.InsereCamposProp
+    PreencheSare.NaoExiste
+    PreencheSare.DesenhaRestauracao
+  end
+  Então("o sistema criará o Projeto SARE") do
+    #
+  end
