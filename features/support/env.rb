e3ef1515c45b8a4cba3d2edcb9b496d8280e2a3a
“LucasKoritar23 <lucas.gimenes23@gmail.com>
2018-05-17 11:08:18 -0300

    Subida

--- features/support/env.rb
+++ features/support/env.rb
@@ -17,5 +17,4 @@ Capybara.configure do |config|
     Capybara.current_driver = :selenium
     config.default_max_wait_time = 10
     Capybara.ignore_hidden_elements = false
-    Capybara.page.driver.browser.manage.window.maximize
 end
