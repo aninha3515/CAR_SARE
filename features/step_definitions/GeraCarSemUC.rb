7d29c3d4c45f3b110ac91b9b0185aa5cb2077e27
“LucasKoritar23 <lucas.gimenes23@gmail.com>
2018-05-18 16:32:43 -0300

    Entrega para Michele

--- features/step_definitions/GeraCarSemUC.rb
+++ features/step_definitions/GeraCarSemUC.rb
@@ -1,8 +1,8 @@
 require 'faker'
 usuario = "55613853720"
 #link = "http://exec-dev01.sma.local/SMA-EST-CAR_test/"
-link = "http://homologacao-sigam.eastus2.cloudapp.azure.com/sma-est-car_test/"
-#link = "http://exec-dev01.sma.local/sigam-adequacao-test/"
+#link = "http://homologacao-sigam.eastus2.cloudapp.azure.com/sma-est-car_test/"
+link = "http://exec-dev01.sma.local/sigam-adequacao-test/"
 #link = "http://exec-dev01.sma.local/sigam-homologacao1"
 
 Dado("que esteja preenchendo o cadastro do CAR") do
@@ -11,7 +11,7 @@ Dado("que esteja preenchendo o cadastro do CAR") do
 end
 
 Quando("preencher todas as informações requeridas") do
-	InsereDados.inicio(nomeCAR = "Def Fazenda " +  Faker::Name.first_name + " SARE")
+	InsereDados.inicio(nomeCAR = "Def Fazenda " +  Faker::Name.first_name)
 	InsereDados.InserePessoa(cpfPessoa = "88258394800", permissao = "Tecnico")
 	InsereDados.InserePessoa(cpfPessoa = "03416907833", permissao = "Representante")
 	InsereDados.AnexaProcuracao
