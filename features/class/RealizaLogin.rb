4d32015049a77d9ee4efcc0944932ab95bd5d04e
“LucasKoritar23 <lucas.gimenes23@gmail.com>
2018-05-29 18:10:33 -0300

    Correcao da validacao do RealizaLogin

--- features/class/RealizaLogin.rb
+++ features/class/RealizaLogin.rb
@@ -4,11 +4,8 @@ class RealizaLogin
 		fill_in('username', :with => usuario)
 		fill_in('password', :with => "a")
 		find(:id, "login").click
-		# analisar o trecho do código abaixo
-		@EntraCAR = find(:id, "ctl00_lblNomPortal").text
-			if @EntraCAR == "Centro de Monitoramento e Avaliação de Programas e Projeto"
-				find(:id, "ctl00_lnkLogo").click
-			end
+		sleep(3)
+		find(:id, "ctl00_lnkLogo").click
 	end
 end
 
--- features/support/env.rb
+++ features/support/env.rb
@@ -15,7 +15,7 @@ end
 
 Capybara.configure do |config|
     Capybara.current_driver = :selenium
-    config.default_max_wait_time = 10
+    config.default_max_wait_time = 30
     Capybara.ignore_hidden_elements = false
     #Capybara.page.driver.browser.manage.window.maximize # - Local
     Capybara.page.driver.browser.manage.window.resize_to(1280,768) # - CodeShip
