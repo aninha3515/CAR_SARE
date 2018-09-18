d0880aed47ef2ccd365a3041b00d5b9f28032ff3
LucasKoritar23 <lucas.gimenes23@gmail.com>
2018-09-18 12:30:29 -0300

    AlteracaoGeraTermo

--- features/step_definitions/GeraTermo.rb
+++ features/step_definitions/GeraTermo.rb
@@ -29,5 +29,5 @@ Dado("que esteja tela de cadastro do Sare") do
   end
   Então("o sistema criará o Projeto SARE") do
     numeroSare = find(:id, "ctl00_conteudo_lblNumeroSARE").text
-    puts("Termo: " + numeroSare + " Gerado no link: " + link)
+    puts("Projeto: " + numeroSare + " Gerado no link: " + link)
   end
