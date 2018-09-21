f3f939ece9c3d08a54d2f1660a29c00de2736188
“LucasKoritar23 <lucas.gimenes23@gmail.com>
2018-09-21 19:41:45 -0300

    MelhoriaHooks para prints

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,12 +1,19 @@
-#Before do
-#puts("Iniciando os testes pelo código automatizado")
-#end
+Before do
+puts("Iniciando os testes pelo código automatizado")
+end
 
 After('@tira_print') do
-    page.save_screenshot('log/shots/temp.png')
         sleep(3)
         find(:id, "ctl00_cmdLogin").click
         find(:link, "aqui").click
         sleep(2)
         puts("Deslogado com sucesso do ambiente!")
+end
+
+After do |scenario|
+    nome_cenario = scenario.name.gsub(/[^A-Za-z0-9 ]/ , '')
+    nome_cenario = nome_cenario.gsub(' ', ' ').downcase!
+    print = "log/shots/#{nome_cenario}.png"
+    page.save_screenshot(print)
+    embed(print, 'image/png', 'Execução')
 end
